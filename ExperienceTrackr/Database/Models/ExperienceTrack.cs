d50a8d954d1f601b6d1f51a4bcdffb6076fc9c26
blazethrower320 <blazethrower320@gmail.com>
2022-10-08 15:30:37 -0500

    Updated Files

---
+++ ExperienceTrackr/Database/Models/ExperienceTrack.cs
@@ -0,0 +1,17 @@
+﻿using ShimmyMySherbet.MySQL.EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperienceTrackr.Database.Models
+{
+    public class ExperienceTrack
+    {
+        [SQLPrimaryKey]
+        public ulong SteamID { get; set; }
+        public uint Experience { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
